4d0197ec7bb414fa619f7b3b8aee7f92a6723f96
devboryung <kangbr16@naver.com>
2021-01-17 19:26:00 +0900

    [update] 동물병원 list 조회 업데이트

--- semiProject/src/com/kh/semi/hospital/controller/HospitalController.java
+++ semiProject/src/com/kh/semi/hospital/controller/HospitalController.java
@@ -1,6 +1,7 @@
 package com.kh.semi.hospital.controller;
 
 import java.io.IOException;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.kh.semi.hospital.model.service.HospitalService;
+import com.kh.semi.hospital.model.vo.Hospital;
+import com.kh.semi.hospital.model.vo.PageInfo;
 
 @WebServlet("/hospital/*")
 public class HospitalController extends HttpServlet {
@@ -37,12 +40,35 @@ public class HospitalController extends HttpServlet {
 		try {
 			HospitalService service = new HospitalService();
 			
+			// 현재 페이지를 얻어와 커리스트링으로 사용할 것.
+			// 쿼리스트링은 서버측에서 파라미터로 인식된다.
+			String cp = request.getParameter("cp");
+			// CurrentPage 사용 이유
+			// 1) 페이징 처리 시 계산에 필요한 값이기 때문
+			// 2) 효율적인 UI/UX를 제공하기 위해서 사용한다(상세조회/북마크...)
+		    // 처음엔 얻어 올 값이 없어서 null 이 된다.
+			
 			// 동물병원 목록 조회**************************************
 			if(command.equals("/list")) {
 				
 				errorMsg = "동물병원 목록 조회 중 오류 발생";
 				
+				// 1) 페이징 처리를 위한 값 계산 Service호출
+				PageInfo pInfo = service.getPageInfo(cp);
+				
+				// 2) 게시글 목록 조회 비즈니스 로직 수행
+				List<Hospital> hList = service.selectHospitalList(pInfo);
+				// pInfo를 가져가는 이유 = 
+				// pInfo에 담겨져있는 currentPage와 limit를 이용해 현재 페이지에 맞는 게시글 목록을 조회하기 위해
+				
+				
+				// 썸네일 추가
+				
+				
 				path = "/WEB-INF/views/hospital/hospitalList.jsp";
+				
+				request.setAttribute("hList", hList);
+				request.setAttribute("pInfo", pInfo);
 				
 				view = request.getRequestDispatcher(path);
 				view.forward(request, response);
@@ -50,15 +76,22 @@ public class HospitalController extends HttpServlet {
 			}
 			
 			// 동물병원 등록 화면 전환 **************************************
-			else if(command.contentEquals("/insertForm")) {
-				path = "/WEB-INF/views/hospital/hospitalInsert.jsp";
-				view = request.getRequestDispatcher(path);
-				view.forward(request, response);
-			}
+						else if(command.contentEquals("/insertForm")) {
+							path = "/WEB-INF/views/hospital/hospitalInsert.jsp";
+							view = request.getRequestDispatcher(path);
+							view.forward(request, response);
+						}
 			
 			
 			
 		}catch(Exception e) {
+			e.printStackTrace();
+			path = "/WEB-INF/views/common/errorPage.jsp";
+			
+			// 에러 메세지를 request객체에 담는다
+			request.setAttribute("errorMsg", errorMsg);
+			view = request.getRequestDispatcher(path);
+			view.forward(request, response);
 			
 		}
 		
--- semiProject/src/com/kh/semi/hospital/model/dao/HospitalDAO.java
+++ semiProject/src/com/kh/semi/hospital/model/dao/HospitalDAO.java
@@ -1,5 +1,103 @@
 package com.kh.semi.hospital.model.dao;
 
+import static com.kh.semi.common.JDBCTemplate.*;
+
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import com.kh.semi.hospital.model.vo.Hospital;
+import com.kh.semi.hospital.model.vo.PageInfo;
+
 public class HospitalDAO {
+	
+	private Statement stmt = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rset = null;
+	private Properties prop = null;
+	
+	public HospitalDAO() {
+		String fileName = HospitalDAO.class.getResource("/com/kh/semi/sql/hospital/hospital-query.xml").getPath();
+		try {
+			prop = new Properties();
+			prop.loadFromXML(new FileInputStream(fileName));
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
+	
+
+	/** 전체 동물병원 수 반환 DAO
+	 * @param conn
+	 * @return listCount
+	 * @throws Exception
+	 */
+	public int getListCount(Connection conn) throws Exception {
+		int listCount =0;
+		String query = prop.getProperty("getListCount");
+		
+		try {
+			stmt = conn.createStatement();
+			rset = stmt.executeQuery(query);
+			
+			while(rset.next()){
+				listCount = rset.getInt(1);
+			}
+		}finally {
+			close(rset);
+			close(stmt);
+		}
+		
+		return listCount;
+	}
+
+
+
+
+	/** 동물병원 목록 조회 DAO
+	 * @param conn
+	 * @param pInfo
+	 * @return
+	 * @throws Exception
+	 */
+	public List<Hospital> selectHospitalList(Connection conn, PageInfo pInfo) throws Exception {
+		List<Hospital> hList = null;
+		String query = prop.getProperty("selectHospitalList");
+		
+		try {
+			// SQL 구문 조건절에 대입할 변수 생성
+			int startRow = (pInfo.getCurrentPage()-1) * pInfo.getLimit()+1;
+			int endRow = startRow + pInfo.getLimit()-1;
+			// 7개의 글 중에서 1페이지에 해당하는 글을 가져옴 : 7~2번째의 글만 가져오게 됨.
+			
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, startRow);
+			pstmt.setInt(2,  endRow);
+			
+			rset = pstmt.executeQuery();
+			
+			hList = new ArrayList<Hospital>();
+			
+			while(rset.next()) {
+				Hospital hospital = new Hospital(rset.getString("HOSP_NM"), 
+						rset.getString("LOCATION2"), rset.getString("PHONE"), 
+						rset.getString("OPENING_TIME"), rset.getString("CLOSING_TIME"));
+				hList.add(hospital);		
+			}
+			
+		}finally {
+			close(rset);
+			close(pstmt);
+			
+		}
+		return hList;
+	}
 
 }
