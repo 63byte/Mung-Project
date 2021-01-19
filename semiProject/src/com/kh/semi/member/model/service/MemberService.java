3ae3c7274748178739e399083a611e932e2bdba3
63_byte <PC@DESKTOP-RNCIKEL>
2021-01-19 21:22:38 +0900

    회원탈퇴 (수정 예정..ㅠ)

--- semiProject/src/com/kh/semi/member/model/service/MemberService.java
+++ semiProject/src/com/kh/semi/member/model/service/MemberService.java
@@ -87,68 +87,68 @@ public class MemberService {
 		return result;
 	}
 
-	
-	/** 비밀번호 변경 Service
-	 * @param loginMember
-	 * @param newPw1
-	 * @param newPw2
-	 * @return
-	 * @throws Exception
-	 */
-	public int updatePw(Member loginMember, String newPwd) throws Exception{
-		Connection conn = getConnection();
-		
-		int result = dao.checkCurrentPw(conn, loginMember);
-		
-		if(result > 0) {
-			loginMember.setMemberPwd(newPwd);
-			
-			result = dao.updatePwd(conn, loginMember);
-			
-			if(result > 0)		commit(conn);
-			else				rollback(conn);
-			
-			
-		}else {	// 현재 비밀번호 불일치
-			result = -1;
-		}
-		
-		close(conn);
-		
-		return result;
-	}
-
 	/**
 	 * 비밀번호 변경 Service
 	 * 
 	 * @param loginMember
 	 * @param newPw1
 	 * @param newPw2
+	 * @return
+	 * @throws Exception
+	 */
+	public int updatePw(Member loginMember, String newPwd) throws Exception {
+		Connection conn = getConnection();
+
+		int result = dao.checkCurrentPw(conn, loginMember);
+
+		if (result > 0) {
+			loginMember.setMemberPwd(newPwd);
+
+			result = dao.updatePwd(conn, loginMember);
+
+			if (result > 0)
+				commit(conn);
+			else
+				rollback(conn);
+
+		} else { // 현재 비밀번호 불일치
+			result = -1;
+		}
+
+		close(conn);
+
+		return result;
+	}
+
+	/**
+	 * 회원 탈퇴 (탈퇴 여부 변경) Service
+	 * 
+	 * @param loginMember
 	 * @return result
 	 * @throws Exception
 	 */
-	/*
-	 * public int updatePw(Member loginMember, String newPw1, String newPw2) throws
-	 * Exception {
-	 * 
-	 * Connection conn = getConnection();
-	 * 
-	 * // 1) 현재 비밀번호가 일치하는지 검사 int result = dao.checkCurrentPw(conn, loginMember);
-	 * 
-	 * // 2) 현재 비밀번호 일치 시 새 비밀번호로 수정 if(result > 0) { // 현재 비밀번호가 일치하는 경우
-	 * 
-	 * // loginMember 비밀번호 필드에 newPwd를 세팅하여 재활용 loginMember.setMemberPwd(newPw1);
-	 * loginMember.setMemberPwd(newPw2);
-	 * 
-	 * result = dao.updatePw(conn, loginMember);
-	 * 
-	 * if(result > 0) commit(conn); else rollback(conn);
-	 * 
-	 * } else { // 현재 비밀번호 불일치 result = -1; }
-	 * 
-	 * close(conn);
-	 * 
-	 * return result; }
-	 */
+	public int updateStatus(Member loginMember) throws Exception {
+		Connection conn = getConnection();
+
+		int result = dao.checkCurrentPw(conn, loginMember); // 재활용
+
+		if (result > 0) {
+
+			result = dao.updateStatus(conn, loginMember.getMemberNo());
+
+			if (result > 0)
+				commit(conn);
+			else
+				rollback(conn);
+
+		} else {
+			// 현재 비밀번호가 일치하지 않을 경우 -1 반환
+			result = -1;
+		}
+
+		close(conn);
+
+		return result;
+	}
 
 }
